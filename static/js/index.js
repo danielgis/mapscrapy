b1d6f0ac5ceb65978404e5247e8ac6fb639dd2b8
Daniel Aguado Huaccharaqui <daniel030891@gmail.com>
2020-01-31 23:01:21 -0500

    Se agrego la funcionalidad para el control de las capas cargadas

--- static/js/index.js
+++ static/js/index.js
@@ -5,12 +5,12 @@ require([
   "esri/layers/FeatureLayer",
   "esri/dijit/Popup", 
   "esri/dijit/PopupTemplate",
+  "esri/request",
   "dojo/parser",
   "dojo/dom-construct",
   "dijit/form/Form", 
   "dijit/layout/AccordionContainer", 
   "dijit/layout/ContentPane", 
-  "dijit/form/TextBox",
   "dojo/domReady!"
 ], 
   function(
@@ -20,6 +20,7 @@ require([
     FeatureLayer,
     Popup, 
     PopupTemplate,
+    esriRequest,
     parser,
     domConstruct
   ) {
@@ -57,14 +58,53 @@ require([
     });
 
   _cargarwms = function() {
+    uuid = Math.random().toString(36).substring(2) + Date.now().toString(36);
     var url = document.getElementById('urlwms').value;
-    var featureLayer = new FeatureLayer(url, {
-      mode: FeatureLayer.MODE_ONDEMAND,
-      outFields: ["*"],
-      infoTemplate: infoTemplate
+
+    var requestHandle = esriRequest({
+      "url": url,
+      "content": {
+        "f": "json"
+      },
+      "callbackParamName": "callback"
     });
-    mapviewer.addLayer(featureLayer);
+    requestHandle.then(
+      function(response){
+        var featureLayer = new FeatureLayer(url, {
+          mode: FeatureLayer.MODE_ONDEMAND,
+          outFields: ["*"],
+          infoTemplate: infoTemplate,
+          id: uuid
+        });
+        mapviewer.addLayer(featureLayer);
+        _listarwms(uuid, response);
+        // console.log(response);
+      }, 
+      function(error){
+        alert(error)
+      }
+    );
+
   };
+
+  _removelayer = function(){
+    var id = event.target.parentElement.attributes.value.value
+    var lyr = mapviewer.getLayer(id)
+    mapviewer.removeLayer(lyr);
+    document.getElementById(id).remove();
+  }
+
+  _listarwms = function(uuid, response){
+    name = response.name;
+    container = document.getElementById("layerscontainer");
+    var row = document.createElement("div");
+    var str = `<div class="namelyr">${name}</div>
+               <div value=${uuid} class="icono" onclick="_removelayer()"><i class="fa fa-minus-circle" style="color: red;"></i></div>`;
+    row.innerHTML = str;
+    row.setAttribute("id", uuid);
+    row.setAttribute("class", 'rowlayer');
+    container.appendChild(row);
+  }
 
   document.getElementById('cargarwms').onclick = _cargarwms;
 
