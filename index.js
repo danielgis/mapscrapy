5b6ef9a7fc9f78ff96c2f4bbeff882136e11d726
danielgis <daniel030891@gmail.com>
2020-03-17 00:36:38 -0500

    se mejoro la distribucion del proyecto; se incluyeron nuevos widgets y servicios de consulta

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require([
   "esri/config",
   "esri/dijit/BasemapGallery", 
   "esri/dijit/HomeButton",
+  "esri/dijit/LocateButton",
   "esri/layers/FeatureLayer",
   "esri/dijit/Popup", 
   "esri/dijit/PopupTemplate",
@@ -15,9 +16,10 @@ require([
   "esri/symbols/SimpleFillSymbol",
   "dojo/parser",
   "dojo/dom-construct",
+  // "esri/dijit/FeatureTable",
   "dijit/form/Form", 
   "dijit/layout/AccordionContainer", 
-  "dijit/layout/ContentPane", 
+  "dijit/layout/ContentPane",
   "dojo/domReady!"
 ], 
   function(
@@ -25,6 +27,7 @@ require([
     esriConfig,
     BasemapGallery, 
     HomeButton,
+    LocateButton,
     FeatureLayer,
     Popup, 
     PopupTemplate,
@@ -36,135 +39,12 @@ require([
     Graphic,
     SimpleFillSymbol,
     parser,
-    domConstruct
+    domConstruct,
+    // FeatureTable
   ) {
   parser.parse();
 
   var graphicAsJsonString;
-
-  var services = [
-    {
-      name_ins: 'Geoidep',
-      name_mser: 'ArcGIS Server',
-      name_serv: 'Geoidep',
-      url: "http://mapas.geoidep.gob.pe/geoidep/rest/services",
-    },
-    {
-      name_ins: 'Ingemmet',
-      name_mser: 'ArcGIS Server',
-      name_serv: 'Geocatmin',
-      url: "https://geocatmin.ingemmet.gob.pe/arcgis/rest/services",
-    },
-    {
-      name_ins: 'Cenepred',
-      name_mser: 'ArcGIS Server',
-      name_serv: 'Sigrid',
-      url: "http://sigrid.cenepred.gob.pe/arcgis/rest/services",
-    },
-    {
-      name_ins: 'Sernanp',
-      name_mser: 'ArcGIS Server',
-      name_serv: 'Geoservicios',
-      url: "http://geoservicios.sernanp.gob.pe/arcgis/rest/services"
-    },
-    {
-      name_ins: 'Minam',
-      name_mser: 'ArcGIS Server',
-      name_serv:'GeoservidorPeru',
-      url: 'http://geoservidorperu.minam.gob.pe/arcgis/rest/services',
-    },
-    {
-      name_ins: 'Minam:Sinia',
-      name_mser: 'Geoserver',
-      name_serv: 'AppSinia',
-      url: 'https://appsinia.analyticsperu.com/geoserver/web/'
-    },
-    {
-      name_ins: 'Serfor',
-      name_mser: 'ArcGIS Server',
-      name_serv: 'GeoSerfor',
-      url: 'https://geo.serfor.gob.pe/geoservicios/rest/services/'
-    },
-    {
-      name_ins:'Autoridad Nacional del Agua',
-      name_mser: 'ArcGIS Server',
-      name_serv:'Geosnirh',
-      url: 'http://geosnirh.ana.gob.pe:6080/arcgis/rest/services'
-    },
-    {
-      name_ins:'Autoridad Nacional del Agua',
-      name_mser: 'ArcGIS Server',
-      name_serv:'GeoANA',
-      url: 'http://geo.ana.gob.pe/arcgis/rest/services/'
-    },
-    {
-      name_ins: 'Osinfor',
-      name_mser: 'ArcGIS Server',
-      name_serv: 'Sisfor',
-      url: 'https://sisfor.osinfor.gob.pe/osinfor/rest/services/'
-    },
-    {
-      name_ins: 'MTC',
-      name_mser: 'ArcGIS Server',
-      name_serv: 'ServiciosGeo',
-      url: 'http://serviciosgeo.mtc.gob.pe/geoservicio/rest/'
-    },
-    {
-      name_ins: 'MTC:AATE',
-      name_mser: 'Geoserver',
-      name_serv: 'Services',
-      url: 'http://200.121.128.47:8080/geoserver/web/'
-    },
-    {
-      name_ins: 'Gore San Martín',
-      name_mser: 'ArcGIS Server',
-      name_serv: 'Portal',
-      url: 'https://portal.regionsanmartin.gob.pe/server/rest/services/'
-    },
-    {
-      name_ins: 'Senace',
-      name_mser: 'ArcGIS Server',
-      name_serv: 'GeoSenace',
-      url: 'https://geosenace.senace.gob.pe:6443/arcgis/rest/services'
-    },
-    {
-      name_ins: 'Instituto Catastral de Lima ICL',
-      name_mser: 'ArcGIS Server',
-      name_serv: 'SIT:ICL',
-      url: 'http://sit.icl.gob.pe/arcgis/rest/services/'
-    },
-    {
-      name_ins: 'Instituto de Estadística e Informática',
-      name_mser: 'ArcGIS Server',
-      name_serv: 'ArcGIS:INEI',
-      url: 'http://arcgis.inei.gob.pe:6080/arcgis/rest/'
-    },
-    {
-      name_ins: 'IGP',
-      name_mser: 'Geoserver',
-      name_serv: 'IdeIGP',
-      url: 'http://ide.igp.gob.pe/geoserver/web/'
-    },
-    {
-      name_ins: 'Senamhi',
-      name_mser: 'Geoserver',
-      name_serv: 'IdepSep',
-      url: 'http://idesep.senamhi.gob.pe/geoserver/web/'
-    },
-    {
-      name_ins: 'Ministerio de Cultura',
-      name_mser: 'Geoserver',
-      name_serv: 'Geoservicios',
-      url: 'https://geoservicios.cultura.gob.pe/geoserver/web/'
-    },
-    {
-      name_ins: 'Municipalidad de San Isidro',
-      name_mser: 'Geoserver',
-      name_serv: 'GeoMunisanIsidro',
-      url: 'http://geo.munisanisidro.gob.pe:8080/geoserver/web/'
-    }
-
-  ]
 
   var popup = new Popup({
     titleInBody: false
@@ -187,6 +67,11 @@ require([
     map: mapviewer
   }, "HomeButton");
   home.startup();
+
+  var geoLocate = new LocateButton({
+    map: mapviewer
+  }, "LocateButton");
+  geoLocate.startup();
 
   _addServicesUrls = function(array){
     var innerhtml = ''
@@ -254,12 +139,14 @@ require([
         });
         mapviewer.addLayer(featureLayer);
 
-        console.log(response);
+        // console.log(response);
 
         _zoomToExtent(uuid);
         _listarwms(uuid, response, url);
 
         _loadMetadata(url, response, uuid);
+
+        // _loadTable(featureLayer);
 
       }, 
       function(error){
@@ -268,6 +155,16 @@ require([
       }
     );
   };
+
+  // _loadTable = function(featurelayer){
+  //     var myFeatureTable = new FeatureTable({
+  //   "featureLayer" : featurelayer,
+  //   "outFields": ["*"],
+  //   "map" : mapviewer
+  // }, 'myTableNode');
+
+  //   myFeatureTable.startup();
+  // }
 
   _loadMetadata = function(urlservice, response, uuid){
     var queryTask = new QueryTask(urlservice);
