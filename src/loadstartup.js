1cb6c67598e7af5fc02c941e87bf256f216cacc9
danielgis <daniel030891@gmail.com>
2020-04-01 23:20:10 -0500

    Se agrego el servicio por regiones para descarga; se elimino el servicio de CEIGA UNMSM

--- src/loadstartup.js
+++ src/loadstartup.js
@@ -1,7 +1,7 @@
 var loadLayer = [
     {
         fuente: 'Johns Hopkins University',
-        name: '<strong>Coronavirus COVID-19</strong><br>Fuente: Johns Hopkins University',
+        name: '<strong>Coronavirus COVID-19</strong><br> Fuente: Johns Hopkins University',
         url: 'https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases/FeatureServer/1',
         turn: true,
         renderer: false,
@@ -10,7 +10,7 @@ var loadLayer = [
     }, 
     {
         fuente: 'Telemática SAC',
-        name: '<strong>Confirmados Lima-Callao</strong><br>Fuente: Telemática SAC',
+        name: '<strong>Confirmados Lima-Callao</strong><br> Fuente: Telemática SAC',
         url: 'https://services8.arcgis.com/2qDHoDLqu6VOlf1y/ArcGIS/rest/services/Distritos_Lima_Callao_Confirmados_COVID/FeatureServer/0',
         turn: true,
         renderer: false,
@@ -18,13 +18,13 @@ var loadLayer = [
         disabledOption: false,
     },
     {
-        fuente: ' Centro de Estudiantes de Ingenieria Geografica (CEIGA - UNMSM)',
-        name: '<strong>Confirmados Por departamento</strong><br>Fuente: CEIGA-UNMSM',
-        url: 'https://services5.arcgis.com/1bv21TZ4HV8MKrvW/ArcGIS/rest/services/COVID19_gdb/FeatureServer/0',
+        fuente: 'Telemática SAC',
+        name: '<strong>Confirmados por región</strong><br> Fuente: Telemática SAC',
+        url: 'https://services8.arcgis.com/2qDHoDLqu6VOlf1y/arcgis/rest/services/Departamentos_covid/FeatureServer/0',
         turn: true,
         renderer: true,
-        rendererField: 'CASOS_COVID',
+        rendererField: 'Casos_confirmados',
         opacity: 0.60,
-        disabledOption: true,
+        disabledOption: false,
     }
 ]
