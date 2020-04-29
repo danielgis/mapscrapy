b4ca5d0eebc7854dd4826c8e3b43b7e54dc540dc
Daniel Ramos Mendoza <heber.daniel.ramos.mendoza@gmail.com>
2020-04-29 00:01:46 -0500

    Se sube estructura de carpetas

---
+++ development/js/Listlayer/Widget.js
@@ -0,0 +1,18 @@
+define(
+  [
+    "js/configMap",
+    "esri/request",
+    "esri/layers/FeatureLayer",
+    "dojo/on",
+    "dojo/dom",
+    "dojo/domReady!"
+  ], function(
+    map,
+    esriRequest,
+    FeatureLayer,
+    on,
+    dom,
+  ){
+
+  }
+);
